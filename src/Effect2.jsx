44c9f54a4aed7a4feee5646c743c78d3503451d5
Travis <travisstew214@gmail.com>
2022-05-04 23:58:04 -0400

    git json-server added globally

--- src/Effect2.jsx
+++ src/Effect2.jsx
@@ -1,5 +1,6 @@
 
-import { useState, useReducer } from 'react';
+import { useState, useReducer, useEffect } from 'react';
+import axios from 'axios';
 
 const initialState = {
   isLoading: false,
@@ -8,17 +9,57 @@ const initialState = {
 }
 
 const reducer = (state, action)=>{
-  return state;
+  console.log('reducer', state, action);
+
+  switch(action.type){
+    case"getArticleSat":
+    return{
+      ...state,
+      isLoading: true
+    }
+    case'getArticleSuccess':
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+
+      }
+
+    case'getArticleFailure':
+      return{
+        ...state,
+        isLoading: false,
+
+      }
+    default: 
+      return state
+  }
+
+
 };
 
 
 function Effect2() {
+
 const [state, dispatch] = useReducer(reducer, initialState)
 console.log("render", state);
 
+useEffect(()=>{
+  dispatch({type:"getArticleSat"});
+  axios.get('http://localhost:3004/posts/1').then((res)=>{
+    
+    console.log(res);
+    dispatch({type: 'getArticleSuccess', payload: res.data}).catch(()=>{
+      dispatch({type: "getArticleFailure"});
+    });
+  })
+},[]);
+
   return (
       <div>
-      
+         <h1>useReducer </h1>
+          {state.isLoading && <div>Loading</div>}
+          {state.data && <div>{state.data.title}</div>}
       </div>
   )
 }
